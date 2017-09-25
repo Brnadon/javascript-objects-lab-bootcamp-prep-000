dc80e3d6272750e19e97179576ddabbfe9579a50
Brandon Brasson <rocksong622@gmail.com>
2017-09-25 21:07:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function deleteFromObjectByKey() {
+
+}
+
+function recipes() {
+
+}
+
+
+Object.assign
+
+var obj = { foo: 'bar' }
+
+var newObj = Object.assign({}, obj)
+
+newObj // { foo: 'bar' }
+
+delete newObj.foo // true
+
+newObj // {}
+
+obj // { foo: 'bar' }
