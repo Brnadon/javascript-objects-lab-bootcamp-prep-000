0c92b6d6da5c352697528ce63d661872916a18f2
Brandon Brasson <rocksong622@gmail.com>
2017-09-25 21:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var recipes = {}
- 
+
  function updateObjectWithKeyAndValue(object, key, value) {
  }
 
  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
  }
- 
+
  function deleteFromObjectByKey(object, key) {
 }
- 
+
  function destructivelyDeleteFromObjectByKey(object, key) {
  } 
